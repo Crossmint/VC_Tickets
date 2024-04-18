0f1cd8e67e1f9adb128b4cc08b970b0f19893cab
Penelope <penelope@paella.dev>
2024-04-17 19:38:07 -0500

    latest update

--- src/pages/buyTicket.tsx
+++ src/pages/buyTicket.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import logo from '/public/mint.svg'; // Replace with your logo file path
-import ticketImage from '/public/ticketImage.jpg' 
+import ticketImage from '/public/ticketImage.png' 
 
 const issueTicket = async (email: string) => {
   const response = await fetch('/api/issue', {
@@ -31,33 +31,45 @@ export default function BuyTicket() {
   return (
     <div style={{minHeight: '100vh', backgroundColor: '#f5f5f5',}}>
      
-      <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', padding: '8rem'}}>
-        <div style={{justifyContent: 'center', minHeight: '40vh', backgroundColor: '#ffffff', padding: '0.5rem', borderRadius: '0.5rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', maxWidth: '400px', width: '100%' }}>
-        <div style={{marginTop: '0.1rem'}}>
+      <div style={{ display: 'flex', alignItems: 'center', margin: '0 auto', flexDirection: 'column', padding: '6rem'}}>
+        <div style={{justifyContent: 'center', minHeight: '40vh', backgroundColor: '##F8F9FB', paddingLeft: '0.5rem', paddingTop: '0rem', paddingBottom: '1.5rem', borderRadius: '0.7rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', maxWidth: '400px', width: '100%' }}>
+        <div style={{marginTop: '0.2rem'}}>
         <Image src={logo} alt="Logo" width={200} height={60} />
-        <div style={{marginTop: '5rem', marginBottom: '2rem', marginLeft:'2.2rem', borderRadius:'.3rem'}}> <Image src={ticketImage} alt="ticketImage" width={320} height={320} style={{ borderRadius: '20px', border: '3px solid #606060'}} /> </div>
+        <div style={{marginTop: '5rem', marginBottom: '2rem', marginLeft:'2.2rem', borderRadius:'.3rem'}}> <Image src={ticketImage} alt="ticketImage" width={320} height={320} style={{ borderRadius: '20px', border: '3px solid #AAC5CA'}} /> </div>
       </div>
           <form onSubmit={handleSubmit}>
-            <div style={{ margin: '1rem' }}>
-              <label style={{ display: 'block', marginTop: '2rem', marginBottom: '0.5rem', fontWeight: 'bold', color: '#333333' }}>Email:</label>
+            <div style={{
+                display: 'flex',
+                flexDirection:'row',
+                alignItems:'center',
+                justifyContent: 'center',
+                margin: '0 auto',
+                cursor: 'pointer',}}><div style={{ margin: '1rem',}}>
+              <label style={{alignItems: 'center', display: 'block', color: '#333333', padding: '.5rem' }}></label>
               <input
                 type="email"
-                placeholder='mail@crossmint.com'
+                placeholder='type your email here'
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                style={{ width: 'calc(100% - 2.1rem)', padding: '0.5rem', border: '1px solid #cccccc', borderRadius: '0.25rem' }}
-              />
+                style={{ width: 'calc(100% - 1.5rem)', padding: '1rem', border: '1px solid #cccccc', borderRadius: '0.25rem' }}
+              /></div>
             </div>
             <button
-              type="submit"
+              type="submit" 
               style={{
-                width: 'calc(100% - 1rem)',
+                width: 'calc(60% - 1rem)',
+                display: 'flex',
+                flexDirection:'row',
+                alignItems:'center',
+                justifyContent: 'center',
                 padding: '0.75rem',
                 backgroundColor: '#04AA6D',
                 color: '#ffffff',
+                fontWeight: 'bold',
                 border: 'none',
                 borderRadius: '0.25rem',
+                margin: '0 auto',
                 cursor: 'pointer',
                 transition: 'background-color 0.3s ease',
               }}
--- src/pages/purchaseCompleted.tsx
+++ src/pages/purchaseCompleted.tsx
@@ -1,22 +1,29 @@
-import React, { useState } from 'react';
-import logo from '/public/mint.svg'; 
+import React, { useState, useEffect } from 'react';
+import logo from '/public/mint.svg';
 import Image from 'next/image';
 
 export default function PurchaseCompleted() {
+  const [currentImage, setCurrentImage] = useState('/loading.gif'); // Start with loading GIF
 
-  
+  useEffect(() => {
+    // Set a timer to switch the image after 6 seconds
+    const timer = setTimeout(() => {
+      setCurrentImage('/checkyes.png'); // Change to the final image
+    }, 15000); // 6000 milliseconds = 6 seconds
 
+    // Clean up the timer when the component unmounts or the image changes
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div style={{ position: 'relative', minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
-     
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: '2rem' }}>
-      <div>
-        <Image src={logo} alt="Logo" width={200} height={200} />
-      </div>
-      
-        <div style={{ backgroundColor: '#ffffff', padding: '2rem', borderRadius: '0.5rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', maxWidth: '400px', width: '100%' }}>
-          <p style={{ width: '100%', textAlign: 'center', fontSize: '1.5rem', color: '#333', marginBottom: '2rem' }}>An email will be sent shortly!</p>
+    <div style={{ position: 'relative', minHeight: '100vh', backgroundColor: '#fff' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingTop: '1rem' }}>
+        <div style={{ backgroundColor: '#F8F9FB', padding: '2rem', borderRadius: '0.5rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', maxHeight: '600px', maxWidth: '400px', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+          <Image src={logo} alt="Logo" width={200} height={60} />
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '1rem', marginBottom: '1rem' }}>
+            <Image src={currentImage} alt="Display Image" width={150} height={150} />
+            <p style={{ fontFamily: 'sans-serif', textAlign: 'center', fontSize: '1rem', color: '#285E63', marginTop: '1rem' }}>An email will be sent shortly!</p>
+          </div>
         </div>
       </div>
     </div>
