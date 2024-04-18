e234c196896596007f1d4142ff9b638e69ed9101
Penelope <penelope@paella.dev>
2024-04-17 19:43:50 -0500

    penelope final ui

--- src/pages/purchaseCompleted.tsx
+++ src/pages/purchaseCompleted.tsx
@@ -9,7 +9,7 @@ export default function PurchaseCompleted() {
     // Set a timer to switch the image after 6 seconds
     const timer = setTimeout(() => {
       setCurrentImage('/checkyes.png'); // Change to the final image
-    }, 15000); // 6000 milliseconds = 6 seconds
+    }, 60000); // 6000 milliseconds = 6 seconds
 
     // Clean up the timer when the component unmounts or the image changes
     return () => clearTimeout(timer);
@@ -17,12 +17,12 @@ export default function PurchaseCompleted() {
 
   return (
     <div style={{ position: 'relative', minHeight: '100vh', backgroundColor: '#fff' }}>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingTop: '1rem' }}>
-        <div style={{ backgroundColor: '#F8F9FB', padding: '2rem', borderRadius: '0.5rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', maxHeight: '600px', maxWidth: '400px', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-          <Image src={logo} alt="Logo" width={200} height={60} />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingTop: '3rem' }}>
+        <div style={{ backgroundColor: '#F8F9FB', padding: '2rem', borderRadius: '0.5rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', maxHeight: '600px', maxWidth: '400px', width: '100%' }}>
+          <Image src={logo} alt="Logo" width={175} height={60} />
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '1rem', marginBottom: '1rem' }}>
-            <Image src={currentImage} alt="Display Image" width={150} height={150} />
-            <p style={{ fontFamily: 'sans-serif', textAlign: 'center', fontSize: '1rem', color: '#285E63', marginTop: '1rem' }}>An email will be sent shortly!</p>
+            <Image src={currentImage} alt="Display Image" width={125} height={125} />
+            <p style={{ fontFamily: 'sans-serif', textAlign: 'center', fontSize: '1.1rem', color: '#285E63', marginTop: '2.5rem' }}>An email will be sent shortly!</p>
           </div>
         </div>
       </div>
